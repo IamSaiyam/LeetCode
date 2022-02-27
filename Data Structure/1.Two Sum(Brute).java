032e2aa276da5f183805c64509319bb6f84fb59d
Saiyam Jain <59947799+saiyamhk@users.noreply.github.com>
2022-02-27 11:44:45 +0530

    Create 1.Two Sum(Brute).java

---
+++ Data Structure/1.Two Sum(Brute).java
@@ -0,0 +1,13 @@
+//saiyamhk
+//57 / 57 test cases passed.
+//Status: Accepted
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0; i<nums.length; i++)
+            for(int j=0; j<nums.length; j++)
+                if(i!=j && nums[i] + nums[j] == target)
+                    return (new int[]{i,j});
+        return (new int[]{-1,-1});
+    }
+}
